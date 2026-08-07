854a436119b2aa42b07662d66b15760cbbb90222
Julien Duchesne <julien.duchesne@elmy.fr>
2026-08-07 11:16:10 +0200

    feat(): new neon design

--- src/content.config.js
+++ src/content.config.js
@@ -1,4 +1,5 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from "astro:content";
+import { z } from "astro:zod";
 import { API_URL } from "astro:env/client";
 
 const alcohols = defineCollection({
@@ -26,26 +27,28 @@ const cocktails = defineCollection({
 
     const cocktails = [];
     for (const element of data) {
-        const response = await fetch(`${API_URL}/cocktails/${element.slug}`);
-        const data = await response.json();
+      const response = await fetch(`${API_URL}/cocktails/${element.slug}`);
+      const data = await response.json();
 
-        cocktails.push(data);
+      cocktails.push(data);
     }
 
-    return cocktails.map(({id, ...cocktail}) => {
-        return ({
-            id: cocktail.slug,
-            ...cocktail,
-        });
+    return cocktails.map(({ id, ...cocktail }) => {
+      return {
+        id: cocktail.slug,
+        ...cocktail,
+      };
     });
   },
   schema: z.strictObject({
     id: z.string(),
-    ingredients: z.array(z.strictObject({
+    ingredients: z.array(
+      z.strictObject({
         measure: z.string().nullable(),
         name: z.string(),
         quantity: z.number(),
-    })),
+      }),
+    ),
     name: z.string(),
     recipeSteps: z.array(z.string()),
     slug: z.string(),
